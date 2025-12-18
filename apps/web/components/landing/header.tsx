aee92eda009e63c0749abef07e4e3d2b8a2b67e9
subhash131 <subhashnayak131@gmail.com>
2025-12-18 06:47:36 +0530

    setup intro

--- apps/web/components/landing/header.tsx
+++ apps/web/components/landing/header.tsx
@@ -6,14 +6,17 @@ import { Button } from "@workspace/ui/components/button";
 
 export const Header = () => {
   return (
-    <div className="h-16 fixed top-0 w-full flex justify-between px-20 items-center backdrop-blur-2xl">
+    <div className="h-16 fixed top-0 w-full flex justify-between px-40 items-center backdrop-blur-2xl">
       <div className="flex gap-4">
         <HugeiconsIcon icon={MagicWand01Icon} size={30} className="rotate-90" />
         <Label className="text-xl font-semibold">AI Store</Label>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
         <Button variant={"link"}>Docs</Button>
         <Button variant={"link"}>GitHub</Button>
+        <Button variant={"default"} className="px-6 py-4 rounded-full">
+          Schedule a Call
+        </Button>
       </div>
     </div>
   );
