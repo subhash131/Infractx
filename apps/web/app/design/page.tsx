568c22be8638144bb7161b10bbd64455640e1976
subhash131 <subhashnayak131@gmail.com>
2025-12-22 16:54:25 +0530

    initial setup

--- apps/web/app/design/page.tsx
+++ apps/web/app/design/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Dashboard</div>;
+  return <div>Page</div>;
 };
 
 export default Page;
---
+++ apps/web/app/files/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@workspace/ui/components/button";
+import React from "react";
+
+const Page = () => {
+  return <div></div>;
+};
+
+export default Page;
