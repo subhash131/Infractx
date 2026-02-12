31bdecdd0d47a8c61b437fd94bc6ead95e873144
subhash131 <subhashnayak131@gmail.com>
2026-02-13 02:31:55 +0530

    fix page switch bug

--- apps/web/app/project/[projectId]/document/[docId]/components/tiptap-editor/editor.tsx
+++ apps/web/app/project/[projectId]/document/[docId]/components/tiptap-editor/editor.tsx
@@ -1,5 +1,6 @@
 import { SimpleEditor } from "./simple-editor/tiptap-templates/simple/simple-editor"
+import { Id } from "@workspace/backend/_generated/dataModel"
 
-export default function Editor() {
-  return <SimpleEditor />
+export default function Editor({textFileId}:{textFileId:Id<"text_files">}) {
+  return <SimpleEditor textFileId={textFileId} />
 }
